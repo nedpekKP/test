b1a45da2c0df2a9865161f6b97b43e4e161087c8
nedpekKP <nedas.pekorius@kinopavasaris.lt>
2025-08-07 00:04:36 +0300

    Update Reset_and_clear_print_spooler.bat

--- Reset_and_clear_print_spooler.bat
+++ Reset_and_clear_print_spooler.bat
@@ -1,9 +1,3 @@
-:: Created by: Shawn Brink
-:: Created on: August 5th 2017
-:: Tutorial: https://www.tenforums.com/tutorials/90615-reset-clear-print-spooler-windows-10-a.html
-
-
-@echo off
-powershell -windowstyle hidden -command "Start-Process cmd -ArgumentList '/s,/c,net stop spooler & DEL /F /S /Q %systemroot%\System32\spool\PRINTERS\* & net start spooler' -Verb runAs"
-
-
+net stop spooler
+DEL /F /S /Q %systemroot%\System32\spool\PRINTERS\*
+net start spooler
